17072e7e3554f2dfd29faf5ce56a24791a88dfad
Léane Barbotin <barbotinleane@gmail.com>
2022-05-16 13:42:26 +0200

    Add viewport meta balise

--- deploy.php
+++ deploy.php
@@ -16,7 +16,8 @@ add('writable_dirs', []);
 host('lagoonformations')
     ->set('remote_user', 'lagoonk')
     ->set('http_user', 'lagoonk')
-    ->set('deploy_path', '~/lagoonFormGit');
+    ->set('deploy_path', '~/lagoonFormGit')
+    ->set('writable_mode', 'chmod');
 
 // Tasks
 task('cache:clear', function () {
