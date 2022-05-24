21f60f39fbbbbfa172de5110ffd3df6005933d37
Léane Barbotin <barbotinleane@gmail.com>
2022-05-24 15:45:07 +0200

    config

--- deploy.php
+++ deploy.php
@@ -13,7 +13,8 @@ add('writable_dirs', []);
 
 // Hosts
 
-host('ssh.cluster031.hosting.ovh.net')
+host('lagoon-formations.com')
+    ->set('hostname', 'ssh.cluster031.hosting.ovh.net')
     ->set('config_file', '~/.ssh/config')
     ->set('remote_user', 'lagoonk')
     ->set('http_user', 'lagoonk')
