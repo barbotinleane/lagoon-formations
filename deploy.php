dbc48a8a6b3a54843f68d2e31b3875e70789d723
Léane Barbotin <barbotinleane@gmail.com>
2022-06-07 15:20:40 +0200

    r

--- deploy.php
+++ deploy.php
@@ -40,7 +40,7 @@ task('pwd', function (): void {
 task('deploy:build:assets', function (): void {
     run('yarn install');
     run('yarn encore production');
-})->local()->desc('Install front-end assets');
+});
 
 before('deploy:symlink', 'deploy:build:assets');
 
