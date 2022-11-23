4cb16eb57171de7e4877bee4deeea49513a86101
Léane Barbotin <barbotinleane@gmail.com>
2022-11-23 10:41:53 +0100

    Change directory to deploy

--- deploy.php
+++ deploy.php
@@ -16,7 +16,7 @@ add('writable_dirs', []);
 host('lagoonformations')
     ->set('remote_user', 'lagoonk')
     ->set('http_user', 'lagoonk')
-    ->set('deploy_path', '~/lagoonsymfony')
+    ->set('deploy_path', '~/testlagoonsymfony')
     ->set('branch', 'prod')
     ->set('writable_mode', 'chmod');
 
